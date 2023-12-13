c6313bc34473b2f49ad87b9376334a2f611613ee
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-12-13 11:14:49 +0700

    Initial Commit

---
+++ lib/pages/on_boarding_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:saving_app/styles/colors.dart';
+import 'package:saving_app/styles/text_style.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: kBgColor.withOpacity(0.5),
+        child: Column(
+          children: [
+            SafeArea(
+              child: Image.asset('assets/images/ob-money.png'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              constraints: const BoxConstraints.expand(
+                height: 300,
+                width: 315,
+              ),
+              decoration: const BoxDecoration(
+                color: kWhite,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome!',
+                    style: kHeading5.copyWith(color: kBlack),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'welcome to Fleet Finance, the easy way to improve your finances and help you control expenses and income',
+                    style: kSubtitle2.copyWith(color: kSuvaGray),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      backgroundColor: kBlueRibbon,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 36, vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: kButton1.copyWith(color: kWhite),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
