0a078ebfe7ae945d36850d6b07b89e1523a70d35
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-12-17 14:55:33 +0700

    Initial Commit

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,14 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:saving_app/styles/colors.dart';
+import 'package:saving_app/styles/text_style.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Home Page'),
-      )
-    );
+    Widget _transactionButton(String icon, String text) {
+      return Expanded(
+        child: Container(
+          constraints: const BoxConstraints.expand(
+            height: 60,
+          ),
+          decoration: const BoxDecoration(
+            color: kNightBlack,
+            borderRadius: BorderRadius.all(
+              Radius.circular(15),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                icon,
+                height: 24,
+                width: 24,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                text,
+                style: kBody1.copyWith(
+                  color: kWhite,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return SafeArea(
+        child: Container(
+      color: kBgColor.withOpacity(0.5),
+      child: Stack(
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30, top: 35),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Welcome,',
+                            style: kBody1.copyWith(
+                              color: kMatterhornBlack,
+                            ),
+                          ),
+                          Text(
+                            'Enna Santana,👋',
+                            style: kHeading6.copyWith(
+                              color: kMatterhornBlack,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        constraints: const BoxConstraints.expand(
+                          height: 40,
+                          width: 40,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/user-img.png'),
+                            fit: BoxFit.cover,
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: kGrey,
+                              blurRadius: 5,
+                              offset: Offset.fromDirection(2),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 28),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    constraints: const BoxConstraints.expand(
+                      height: 170,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/bg-container.png'),
+                        fit: BoxFit.cover,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: kGrey,
+                          blurRadius: 5,
+                          offset: Offset.fromDirection(10),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'My Saving',
+                          style: kSubtitle2.copyWith(
+                            color: kWhite,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Rp. 10.430.000',
+                          style: kHeading5.copyWith(
+                            color: kWhite,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        LinearPercentIndicator(
+                          lineHeight: 4,
+                          padding: const EdgeInsets.symmetric(horizontal: 0),
+                          progressColor: kEgyptianBlue,
+                          percent: 0.5,
+                          backgroundColor: kWhite,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Text('Rp. 10.430.000 / Rp. 40.000.000',
+                            style: kCaption.copyWith(
+                              color: kWhite,
+                            )),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    children: [
+                      _transactionButton(
+                        'assets/icons/save.png',
+                        'Save Money',
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      _transactionButton(
+                        'assets/icons/pay.png',
+                        'Pay',
+                      ),
+                    ],
+                  )
+                ],
+              )),
+        ],
+      ),
+    ));
   }
 }
