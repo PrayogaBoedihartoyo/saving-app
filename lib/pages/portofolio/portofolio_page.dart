db94384d1f359b8514e50968f3631882f6903351
Prayoga Boedihartoyo <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2023-12-22 10:57:57 +0700

    Initial Commit

--- lib/pages/portofolio/portofolio_page.dart
+++ lib/pages/portofolio/portofolio_page.dart
@@ -7,12 +7,7 @@ class PortfolioPage extends StatelessWidget {
   const PortfolioPage({super.key});
 
   Widget _portofolioCardList(
-      String icon,
-      String title,
-      double percent,
-      String amount,
-      String time
-      ) {
+      String icon, String title, double percent, String amount, String time) {
     return Container(
       margin: const EdgeInsets.only(
         left: 30,
@@ -93,83 +88,105 @@ class PortfolioPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(children: [
-        Container(
-          padding: const EdgeInsets.only(top: 25),
-          height: 240,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.vertical(
-              bottom: Radius.circular(40),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 25),
+            height: 240,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.vertical(
+                bottom: Radius.circular(40),
+              ),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/bg-container-2.png'),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: kGrey,
+                    blurRadius: 5,
+                    offset: Offset.fromDirection(2)),
+              ],
             ),
-            image: const DecorationImage(
-              image: AssetImage('assets/images/bg-container-2.png'),
-              fit: BoxFit.cover,
+            child: SafeArea(
+              child: Column(children: [
+                Text(
+                  'My Porfolio',
+                  style: kHeading6.copyWith(
+                      color: kWhite, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Savings Value',
+                  style: kSubtitle2.copyWith(color: kWhite),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text('Rp 12.480.000',
+                    style: kHeading5.copyWith(
+                        color: kWhite, fontWeight: FontWeight.w600)),
+              ]),
             ),
-            boxShadow: [
-              BoxShadow(
-                  color: kGrey, blurRadius: 5, offset: Offset.fromDirection(2)),
-            ],
           ),
-          child: SafeArea(
-            child: Column(children: [
-              Text(
-                'My Porfolio',
-                style: kHeading6.copyWith(
-                    color: kWhite, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Savings Value',
-                style: kSubtitle2.copyWith(color: kWhite),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Text('Rp 12.480.000',
-                  style: kHeading5.copyWith(
-                      color: kWhite, fontWeight: FontWeight.w600)),
-            ]),
+          _portofolioCardList(
+            'assets/icons/pension.png',
+            'Pension savings funds',
+            0.3,
+            'Rp. 10.430.000 / Rp. 40.000.000',
+            'Last saving February 19',
           ),
-        ),
-        _portofolioCardList(
-          'assets/icons/pension.png',
-          'Pension savings funds',
-          0.3,
-          'Rp. 10.430.000 / Rp. 40.000.000',
-          'Last saving February 19',
-        ),
-        _portofolioCardList(
-          'assets/icons/camera.png',
-          'Camera',
-          0.5,
-          'Rp. 2.050.000 / Rp. 4.000.000',
-          'Last saving February 16',
-        ),
-        _portofolioCardList(
-          'assets/icons/camera.png',
-          'Camera',
-          0.5,
-          'Rp. 2.050.000 / Rp. 4.000.000',
-          'Last saving February 16',
-        ),
-        _portofolioCardList(
-          'assets/icons/camera.png',
-          'Camera',
-          0.5,
-          'Rp. 2.050.000 / Rp. 4.000.000',
-          'Last saving February 16',
-        ),
-        _portofolioCardList(
-          'assets/icons/camera.png',
-          'Camera',
-          0.5,
-          'Rp. 2.050.000 / Rp. 4.000.000',
-          'Last saving February 16',
-        ),
-      ]),
+          _portofolioCardList(
+            'assets/icons/camera.png',
+            'Camera',
+            0.5,
+            'Rp. 2.050.000 / Rp. 4.000.000',
+            'Last saving February 16',
+          ),
+          _portofolioCardList(
+            'assets/icons/camera.png',
+            'Camera',
+            0.5,
+            'Rp. 2.050.000 / Rp. 4.000.000',
+            'Last saving February 16',
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 30,
+            ),
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                backgroundColor: kWhite,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.add,
+                    size: 13,
+                    color: kLuckyBlue,
+                  ),
+                  Text(
+                    'Add Portofolio',
+                    style: kBody2.copyWith(
+                      color: kLuckyBlue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
